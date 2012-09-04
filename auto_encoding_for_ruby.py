313344717b822f394fc4dcb22940b6d87c5b0005
Lee Dohm <lee@liftedstudios.com>
2012-09-03 20:33:09 -0700

    Make the encoding format and match pattern configurable.

--- auto_encoding_for_ruby.py
+++ auto_encoding_for_ruby.py
@@ -1,8 +1,23 @@
+# 
+# Copyright (c) 2012 by Lifted Studios.  All Rights Reserved.
+# 
+
+import constants
 import re
 import sublime
 import sublime_plugin
 
 class AutoEncodingForRuby(sublime_plugin.EventListener):
+  '''
+  Automatically adds or removes an encoding declaration, if necessary, at the top of the file.
+  '''
+
+  def __init__(self):
+    '''
+    Initializes the plugin.
+    '''
+    self.settings = sublime.load_settings(constants.SETTINGS_FILE)
+
   def on_load(self, view):
     self.handle_encoding_declaration_on(view)
 
@@ -29,14 +44,15 @@ class AutoEncodingForRuby(sublime_plugin.EventListener):
     file_content.decode("ascii")
 
   def has_encoding_declaration_on_first_line_of(self, view):
-    return re.search("^\s*#\s*encoding\s*:\s*utf-8\s*$", self.first_line_from(view), re.IGNORECASE)
+    pattern = self.settings.get(constants.ENCODING_PATTERN)
+    return re.match(pattern, self.first_line_from(view), re.IGNORECASE)
 
   def first_line_from(self, view):
     return view.substr(view.full_line(0))
 
   def add_encoding_declaration_on_the_first_line_of(self, view):
     edit = view.begin_edit()
-    view.insert(edit, 0, "#encoding: utf-8\n\n")
+    view.insert(edit, 0, self.settings.get(constants.SETTING_FORMAT) + "\n\n")
     view.end_edit(edit)
 
   def remove_encoding_declaration_on_the_first_line_of(self, view):
---
+++ constants.py
@@ -0,0 +1,8 @@
+# 
+# Copyright (c) 2012 by Lifted Studios.  All Rights Reserved.
+# 
+
+ENCODING_PATTERN = "#.*coding:\s*utf-8"
+PLUGIN_NAME = 'Auto Encoding For Ruby'
+SETTING_FORMAT = 'format'
+SETTINGS_FILE = PLUGIN_NAME + '.sublime-settings'
